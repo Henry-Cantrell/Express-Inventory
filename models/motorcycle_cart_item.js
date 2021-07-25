cb70da2ad4e9d51f6fdd3bc9f9bd6d7df8ff8780
Henry <henry@Henrys-MacBook-Pro.local>
2021-07-25 12:11:57 -0400

    Motorcycle cart additions work

--- models/motorcycle_cart_item.js
+++ models/motorcycle_cart_item.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 var MotorcycleCartItemSchema = new Schema(
   {
     user: {type: Schema.ObjectId, ref: 'User', required: true},
-    motorcycle: {type: Schema.ObjectId, ref: 'Motorcycle', required:true}
+    motorcycle: {type: Schema.ObjectId, ref: 'Motorcycle', required: true}
   }
 );
 
--- routes/cart.js
+++ routes/cart.js
@@ -4,12 +4,12 @@ var router = express.Router();
 // Controller imports
 
 var gear_cart_item_controller = require('../controllers/gearCartItemController');
-//var motorcycle_cart_item_controller = require('../controllers/motorcycleCartItemController');
+var motorcycle_cart_item_controller = require('../controllers/motorcycleCartItemController');
 
 // Handle posts for motorcycle and gear item creations
 
 router.post("/gear/add", gear_cart_item_controller.gearCartItem_create_post);
-//router.post("/motorcycle/add", motorcycle_cart_item_controller.motorcycleCartItem_create_post);
+router.post("/motorcycle/add", motorcycle_cart_item_controller.motorcycleCartItem_create_post);
 
 // Handle deletion of motorcycle and gear items
 
